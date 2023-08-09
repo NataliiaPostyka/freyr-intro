da43aee734334c759d1619d10132aaf32ca89901
Nataliia Postyka <63284405+NataliiaPostyka@users.noreply.github.com>
2023-08-09 08:27:51 -0700

    4-3-lesson

--- js/index.js
+++ js/index.js
@@ -2,19 +2,19 @@
 const today = new Date()
 const thisYear = today.getFullYear()
 
-// Select the footer element
+//footer element
 const footer = document.querySelector("footer")
 
-// Create a new paragraph element
+//paragraph element
 const copyright = document.createElement("p")
 
-// Set the inner HTML of the paragraph to display the copyright text
+//copyright text
 copyright.innerHTML = `&copy; ${thisYear} Nataliia Postyka`
 
 // Append the paragraph to the footer
 footer.appendChild(copyright)
 
-// List of technical skills
+//skills
 const skills = [
   "Quality Assurance (QA)",
   "Automation Testing",
@@ -24,15 +24,53 @@ const skills = [
   "Backend Development",
 ]
 
-// Select the skills section
 const skillsSection = document.getElementById("skills")
 
-// Select the skills list
 const skillsList = skillsSection.querySelector("ul")
 
-// Loop through the skills array and create list items for each skill
 for (const skill of skills) {
   const skillListItem = document.createElement("li")
   skillListItem.textContent = skill
   skillsList.appendChild(skillListItem)
 }
+
+// DOM Selection: Select the form and add a submit event listener
+const messageForm = document.forms.leave_message
+messageForm.addEventListener("submit", function (event) {
+  // Prevent form from refreshing the page
+  event.preventDefault()
+
+  // Retrieve form field values
+  const usersName = messageForm.usersName.value
+  const usersEmail = messageForm.usersEmail.value
+  const usersMessage = messageForm.usersMessage.value
+
+  // Log the form field values
+  console.log("Name:", usersName)
+  console.log("Email:", usersEmail)
+  console.log("Message:", usersMessage)
+
+  // Clear the form fields
+  messageForm.reset()
+
+  // Create new message entry
+  const messageSection = document.querySelector("#messages")
+  const messageList = messageSection.querySelector("ul")
+
+  const newMessage = document.createElement("li")
+  newMessage.innerHTML = `
+    <a href="mailto:${usersEmail}">${usersName}</a>
+    <span>${usersMessage}</span>
+  `
+
+  const removeButton = document.createElement("button")
+  removeButton.textContent = "remove"
+  removeButton.type = "button"
+  removeButton.addEventListener("click", function () {
+    const entry = removeButton.parentNode
+    entry.remove()
+  })
+
+  newMessage.appendChild(removeButton)
+  messageList.appendChild(newMessage)
+})
